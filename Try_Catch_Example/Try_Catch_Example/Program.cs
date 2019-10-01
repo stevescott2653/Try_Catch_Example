874e91cfe41789d0d04ea76e789dfc1e700077aa
Steven Scott <stevescott2653@gmail.com>
2019-10-01 17:05:26 -0400

    Add files via upload

---
+++ Try_Catch_Example/Try_Catch_Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Try_Catch_Example
+{
+    public class ExExample
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 0;
+            int x = a/b;
+            Console.WriteLine("Rest of the code");
+        }
+    }
+}
